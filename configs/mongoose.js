b09d84c377471b9abfbb6ff275985e262efb34a3
Paolo <fedeli.paolol@gmail.com.com>
2020-02-26 10:48:06 +0100

    done

--- configs/mongoose.js
+++ configs/mongoose.js
@@ -3,3 +3,20 @@ const mongoose = require("mongoose");
 
 
 
+//MONGOOSE CONNECTION
+mongoose
+  .connect(
+    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-xc8rg.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  )
+  .then(x => {
+    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+  })
+  .catch(err => {
+    console.error("Error connecting to mongo", err);
+  });
+
+module.exports = mongoose;
--- server.js
+++ server.js
@@ -1,36 +1,16 @@
-
-
 const express = require("express");
-const swaggerJSDoc = require('swagger-jsdoc')
-const swaggerUi = require ("swagger-ui-express");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
 
 const app = express();
 require("dotenv").config();
 require("./configs/middlewares")(app);
-
-//MONGOOSE CONNECTION
-mongoose
-  .connect(
-    `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-xc8rg.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }
-  )
-  .then(x => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
-  })
-  .catch(err => {
-    console.error("Error connecting to mongo", err);
-  });
-
-module.exports = mongoose;
+require("./configs/mongoose");
+require("./configs/swagger");
 
 const API_PORT = process.env.PORT || 3000;
-
 
 //SWAGGER CONFIG
 
@@ -57,7 +37,8 @@ app.get("/swagger.json", (req, res) => {
   res.send(swaggerSpec);
 });
 
-app.use(cors())
+app.use(cors());
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
 app.use("/users", require("./routes/createUser"));
@@ -68,12 +49,7 @@ app.use("/users", require("./routes/getUsers"));
 
 app.use("/users", require("./routes/deleteUsersById"));
 
-app.use("/users", require("./routes/updateUsersById"))
-
-
-
-
-
+app.use("/users", require("./routes/updateUsersById"));
 
 // eslint-disable-next-line no-console
 app.listen(API_PORT, () => console.log(`Listening on port ${API_PORT}`));
